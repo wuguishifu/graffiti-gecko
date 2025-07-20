10e94bc43a7884898b861323fc9498d145277bec
Bo Bramer <bramer.bo@gmail.com>
2025-07-20 16:38:48 -0700

    main menu

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -5,11 +5,14 @@ export function MainMenu() {
         className='absolute top-0 left-0 w-full h-full -z-10 select-none pointer-events-none'
         src='/assets/screens/start.png'
       />
-      <a href='/game' className='w-72 py-4 border rounded-xl flex justify-center cursor-pointer bg-gray-900 hover:bg-gray-800 transition-colors'>
-        <h1 className='text-2xl'>
-          Start
-        </h1>
-      </a>
+      <div className='text-center absolute top-[50%] left-[30%] flex flex-col gap-8'>
+        <a href='/game' className='hover:scale-110 transition-transform'>
+          <img src='/assets/copy/start-run.svg' />
+        </a>
+        <a href='/settings' className='hover:scale-110 transition-transform'>
+          <img src='/assets/copy/settings.svg' />
+        </a>
+      </div>
     </main>
   );
 }
