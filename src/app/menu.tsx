0d4bdaba8c591c9123435ac26aa303f5e1582584
Bo Bramer <bramer.bo@gmail.com>
2025-07-20 16:09:38 -0700

    update file names and add font file

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -1,6 +1,10 @@
 export function MainMenu() {
   return (
     <main className='h-full flex items-center justify-center'>
+      <img
+        className='absolute top-0 left-0 w-full h-full -z-10 select-none pointer-events-none'
+        src='/assets/screens/start.png'
+      />
       <a href='/game' className='w-72 py-4 border rounded-xl flex justify-center cursor-pointer bg-gray-900 hover:bg-gray-800 transition-colors'>
         <h1 className='text-2xl'>
           Start
