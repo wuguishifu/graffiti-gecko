3ff588e107dde184cb2c4e7632ca560db1b181da
Bo Bramer <bramer.bo@gmail.com>
2025-07-20 17:12:41 -0700

    game over screen

--- src/app/menu.tsx
+++ src/app/menu.tsx
@@ -2,7 +2,7 @@ export function MainMenu() {
   return (
     <main className='h-full flex items-center justify-center'>
       <img
-        className='absolute top-0 left-0 w-full h-full -z-10 select-none pointer-events-none'
+        className='absolute top-0 left-0 w-full h-full -z-10 select-none pointer-events-none object-cover'
         src='/assets/screens/start.png'
       />
       <div className='text-center absolute top-[50%] left-[30%] flex flex-col gap-8'>
