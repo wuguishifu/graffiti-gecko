68bf47780489e7c014908ada7834a94d572c453d
Bo Bramer <bramer.bo@gmail.com>
2025-07-25 21:29:39 -0700

    controls in settings menu

--- src/ui/settings-menu.tsx
+++ src/ui/settings-menu.tsx
@@ -16,30 +16,22 @@ export function SettingsMenu({ onHide }: { onHide: () => void }) {
   const soundVolume = useAppSelector((state) => state.data.soundVolume);
 
   return (
-    <div className="absolute top-0 right-0 w-full h-full flex items-center justify-center bg-white/20 backdrop-blur-md z-50">
+    <div
+      className="absolute top-0 right-0 w-full h-full flex items-center justify-center bg-white/20 backdrop-blur-md z-50"
+      onClick={onHide}
+    >
       <div
+        onClick={(e) => e.stopPropagation()}
         className="bg-[#FD50B4] p-16 rounded-lg flex flex-col items-center"
         style={{
           filter: 'drop-shadow(15px 15px 4px rgba(0, 0, 0, 0.60))',
         }}
       >
-        <img src="/assets/copy/settings-title.svg" />
-        <div className="mt-24 flex items-center gap-4">
-          <p className="text-6xl font-blank-river !text-black">Spray Color</p>
-          <div className="flex items-center gap-2">
-            {colors.map((color) => (
-              <button
-                tabIndex={-1}
-                key={color}
-                className={`size-[54px] rounded-full border-3 ${selectedColor === color ? 'border-white' : 'border-black'} select-none cursor-pointer`}
-                style={{ backgroundColor: color }}
-                onClick={changeColorFn(color)}
-              />
-            ))}
-          </div>
-        </div>
+        <img src="/assets/copy/settings-title.svg" className="select-none pointer-events-none" />
         <div className="mt-12 flex items-center gap-4 w-full">
-          <p className="text-6xl font-blank-river !text-black">Music</p>
+          <div className="w-44">
+            <img src="/assets/copy/music.svg" className="select-none pointer-events-none" />
+          </div>
           <label
             htmlFor="minmax-range"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -57,7 +49,9 @@ export function SettingsMenu({ onHide }: { onHide: () => void }) {
         </div>
 
         <div className="mt-12 flex items-center gap-4 w-full">
-          <p className="text-6xl font-blank-river !text-black">Sound</p>
+          <div className="w-44">
+            <img src="/assets/copy/sounds.svg" className="select-none pointer-events-none" />
+          </div>
           <label
             htmlFor="minmax-range"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -74,9 +68,59 @@ export function SettingsMenu({ onHide }: { onHide: () => void }) {
           />
         </div>
 
+        <div className="mt-12 flex items-center gap-4 justify-between w-full">
+          <img src="/assets/copy/spray-color.svg" className="select-none pointer-events-none" />
+          <div className="flex items-center gap-2">
+            {colors.map((color) => (
+              <button
+                tabIndex={-1}
+                key={color}
+                className={`size-[54px] rounded-full border-3 ${selectedColor === color ? 'border-white' : 'border-black'} select-none cursor-pointer`}
+                style={{ backgroundColor: color }}
+                onClick={changeColorFn(color)}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="mt-12 flex items-start gap-4 w-full select-none pointer-events-none">
+          <img src="/assets/copy/gameplay.svg" />
+          <div className="w-52 bg-[#FFF600] aspect-square border-3 border-black rounded-xl flex flex-col items-center py-2 px-2 justify-evenly">
+            <img src="/assets/copy/move.svg" />
+            <div className="flex w-full justify-evenly">
+              {/* don't judge me this is just so it lines up quick and dirty */}
+              <div className="bg-transparent border-3 border-transparent rounded-lg aspect-square size-15"></div>
+              <div className="bg-white border-3 border-black rounded-lg aspect-square flex items-center justify-center size-15">
+                <span className="font-blank-river !text-black text-4xl">W</span>
+              </div>
+              <div className="bg-transparent border-3 border-transparent rounded-lg aspect-square size-15"></div>
+            </div>
+            <div className="flex w-full justify-evenly">
+              <div className="bg-white border-3 border-black rounded-lg aspect-square flex items-center justify-center size-15">
+                <span className="font-blank-river !text-black text-4xl">A</span>
+              </div>
+              <div className="bg-white border-3 border-black rounded-lg aspect-square flex items-center justify-center size-15">
+                <span className="font-blank-river !text-black text-4xl">S</span>
+              </div>
+              <div className="bg-white border-3 border-black rounded-lg aspect-square flex items-center justify-center size-15">
+                <span className="font-blank-river !text-black text-4xl">D</span>
+              </div>
+            </div>
+          </div>
+          <div className="w-52 bg-[#FFF600] aspect-square border-3 border-black rounded-xl flex flex-col items-center py-2 justify-evenly">
+            <img src="/assets/copy/start-tag.svg" />
+            <img src="/assets/icons/mouse-click.png" className="h-15" />
+            <div className="flex justify-center w-full">
+              <div className="bg-white border-3 border-black rounded-lg flex items-center justify-center h-15 w-3/4">
+                <span className="font-blank-river !text-black text-4xl">Space</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div className="mt-12 flex justify-end w-full">
           <button
-            className="hover:scale-110 transition-transform cursor-pointer"
+            className="hover:scale-110 transition-transform cursor-pointer select-none"
             onMouseEnter={() => soundService.playSound('hover')}
             onClick={() => {
               soundService.playSound('click');
